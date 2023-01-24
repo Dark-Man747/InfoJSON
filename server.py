6794c6366218c81aac593abea5a14d80906ea8f7
Dark Man <95143027+Dark-Man747@users.noreply.github.com>
2023-01-24 17:29:02 +0400

    Update server.py

--- server.py
+++ server.py
@@ -1,38 +1,29 @@
 from flask import Flask, jsonify
-import requests
+from bs4 import BeautifulSoup
+import requests, random
 app = Flask(__name__)
-
 @app.route('/')
 def index():
-    return jsonify({'Warning': 'Put after the link /user= here, put your Instagram username'})
-
-@app.route('/user=<string:parameter>')
-def get_value(parameter):
-    url = f"https://www.instagram.com/{parameter}/?__a=1&__d=dis"
-    head = {
-        'accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-        'accept-encoding':'gzip,deflate,br',
-        'accept-language':'en-US,en;q=0.9,ar;q=0.8',
-        'cookie':'ig_did=77A45489-9A4C-43AD-9CA7-FA3FAB22FE24;ig_nrcb=1;csrftoken=VOPH7fUUOP85ChEViZkd2PhLkUQoP8P8;mid=YGwlfgALAAEryeSgDseYghX2LAC-',
-        'user-agent': 'Instagram 9.4.0 Android (30/11; 480dpi; 1080x2158; OPPO; CPH2069; OP4C7BL1; qcom; en_US; 276028020)'#generate_user_agent(),
-        }
-    information = requests.get(url,headers=head).json()
-
-    id = (information['graphql']['user']['id'])
-    name = (information['graphql']['user']['full_name'])
-    Bio = (information['graphql']['user']['biography'])
-    Followers = (information['graphql']['user']['edge_followed_by']['count'])
-    Following = (information['graphql']['user']['edge_follow']['count'])
-    url = (information['graphql']['user']['external_url'])
-    private = (information['graphql']['user']['is_private'])
-    business = (information['graphql']['user']['is_business_account'])
-    posts = str(information['graphql']['user']["edge_owner_to_timeline_media"]["count"])
-    re = requests.get(f"https://o7aa.pythonanywhere.com/?id={id}",headers=head)
-    ree = re.json()
-    dat = ree['date']
-    
-    data = {'id': f'{id}', 'name': f'{name}', 'bio': f'{Bio}', 'followers': f'{Followers}', 'following': f'{Following}', 'link': f'{url}', 'private': f'{private}', 'business': f'{business}', 'posts': f'{posts}', 'date': f'{dat}'}
-    return jsonify(data)
-
+    return "Error"
+@app.route('/img=<string:imgs>')
+def get_value(imgs):
+ mod_1 = ['01','02','03','04','05','06','07','08','09','10','11','12']
+ mod_2 = ['01','02','03','04','05','06','07','08','09','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24','25','26','27','28','29','30','31']
+ running = True
+ while running:
+   mod_4 = str(''.join((random.choice(mod_1) for i in range(1))))
+   mod_5 = str(''.join((random.choice(mod_2) for i in range(1))))
+   dateimg = (mod_4+'-'+mod_5)
+   url = f'https://telegra.ph/{imgs}-{dateimg}'
+   response = requests.get(url)
+   soup = BeautifulSoup(response.content, 'html.parser')
+   img_tags = soup.find_all('img')
+   urls = [img['src'] for img in img_tags]
+   for link in urls:
+    if ('/file/') in link:
+     running = False
+     img = ('https://telegra.ph/'+link)
+     data = {'image': f'{img}'}
+     return jsonify(data)
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0', port=1337)
